c387be1555cbd56fa57c1b070e44ef91571217b4
Christinchick <alexey@skyward-crimea.ru>
2022-01-10 17:54:43 +0300

    New add

--- src/test/java/DemoqaTest/Configuration/StartDemoqaFormRegistrationTests.java
+++ src/test/java/DemoqaTest/Configuration/StartDemoqaFormRegistrationTests.java
@@ -8,8 +8,8 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class StartDemoqaFormRegistrationTests {
     @Test
